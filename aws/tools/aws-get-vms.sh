1a75cb722b7b98f813007f0033a56cbd330a0279
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-05-23 16:47:28 +0800

    Update aws-get-vms.sh

--- aws/tools/aws-get-vms.sh
+++ aws/tools/aws-get-vms.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],InstanceType,State.Name,PublicIpAddress,VpcId,LaunchTime]' --output table
+
+# simple
+#aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],InstanceType,State.Name,PublicIpAddress,VpcId,LaunchTime]' --output table
+
+aws ec2 describe-instances \
+    --query 'Reservations[].Instances[].{InstanceId:InstanceId, Name: Tags[?Key==`Name`]|[0].Value, State:State.Name, LaunchTime:LaunchTime, PrivateIP: PrivateIpAddress, PublicIpAddress:PublicIpAddress, Size: InstanceType, PublicDNS: PublicDnsName}' \
+    --output table
+
