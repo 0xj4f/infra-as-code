3c38c6f6ab43bbab76577f78003ae11b538e02a2
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-06-14 13:50:54 +0800

    Create aws-create-user-ses.sh

---
+++ aws/tools/aws-create-user-ses.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Variables
+IAM_USERNAME="aws.email"
+POLICY_ARN="arn:aws:iam::aws:policy/AmazonSESFullAccess"
+
+# Create a new IAM user
+aws iam create-user --user-name $IAM_USERNAME
+
+# Attach the policy to the user
+aws iam attach-user-policy --user-name $IAM_USERNAME --policy-arn $POLICY_ARN
+
+echo "User $IAM_USERNAME created with policy $POLICY_ARN"
+
+aws iam create-access-key --user-name $IAM_USERNAME
