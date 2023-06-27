4e0ce11988c87c8ab891ba867ed377bfa757f7d5
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-06-28 00:17:44 +0800

    Update az-keyvault-manager.py

--- azure/python-sdk/az-keyvault-manager.py
+++ azure/python-sdk/az-keyvault-manager.py
@@ -1,6 +1,32 @@
 from azure.identity import ClientSecretCredential
 from azure.keyvault.secrets import SecretClient
 
+"""
+requirements:
+1. get vault url
+az keyvault show -n demo-keyvault
+
+2. create Service Principal
+az ad sp create-for-rbac --name <http://my-key-vault-principal-name> --sdk-auth
+{
+  "clientId": "xxx",
+  "clientSecret": "xxx",
+  "subscriptionId": "xxx",
+  "tenantId": "xxx",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+get clientID
+
+3. assign rbac 
+az keyvault set-policy -n demo-keyvault --spn $clientID --secret-permissions delete get list set --key-permissions create decrypt delete encrypt get list unwrapKey wrapKey
+"""
+
+
 class KeyVaultSecretManager:
     def __init__(self, tenant_id, client_id, client_secret, vault_url):
         self.credential = ClientSecretCredential(
