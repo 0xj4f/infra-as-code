b38c636fddf7b490d46ef2c0ff44cbef81d3c06f
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-07-31 19:24:04 +0800

    Create create-keyvault-and-secrets.sh

---
+++ azure/scripts/create-keyvault-and-secrets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Assign variables
+resource_group="demo_rg"
+location="northeurope" # Replace with your preferred location
+keyvault_name="demo_project_keyvault"
+secret_name="demo_secret"
+secret_value="demo_value"
+
+# Create Resource Group
+az group create \
+    --name $resource_group \
+    --location $location
+
+# Create Key Vault
+az keyvault create \
+    --name $keyvault_name \
+    --resource-group $resource_group \
+    --location $location
+
+# Set Secret in Key Vault
+az keyvault secret set \
+    --vault-name $keyvault_name \
+    --name $secret_name \
+    --value $secret_value
+
+echo "Created Resource Group: $resource_group"
+echo "Created Key Vault: $keyvault_name"
+echo "Created Secret: $secret_name"
